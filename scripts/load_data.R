c24140237d537e7e9f4f0006090ae3aaa8e02112
tamta117 <tamtar1@uw.edu>
2022-04-08 17:13:27 -0700

    update code and write data into folder

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -1,10 +1,13 @@
 #reformat og datasets
 library(here)
 library(tidyverse)
+library(lubridate)
+library(dplyr)
 
 #load data
 c19_dir<-read_excel(here("data/2019_Carcass_Data_Marzluff.xlsx"))
 c15_dir<-read_excel(here("data/CougarKills_2015.2017.xlsx"))
+stats_dir<-read_csv(here("data/cougardemostats.csv"))
 
 #filter for coug and reformat data
 c19<-c19_dir%>%
@@ -24,3 +27,49 @@ c15$kill_num<-as.character(c15$kill_num)
 #combine data tables
 c15_19<-bind_rows(c15,c19)
 c15_19<-c15_19[complete.cases(c15_19[,1:6]), ] #remove NA
+c15_c19<-parse_date_time(c15_19$dod,orders = c('ymd','dmy'),tz="")
+c15_19<-c15_19%>%
+  mutate(year=year(dod),
+         month=month(dod),
+         day=day(dod),
+         jday=yday(dod))
+c15_19$id<-gsub("MTN LION", "", as.character(c15_19$id))
+c15_19$id<-gsub("MT LION", "", as.character(c15_19$id))
+c15_19$id<-gsub("MT. LION", "", as.character(c15_19$id))
+
+#reformat demographics data
+stats<-stats_dir%>%
+  select(ID,Sex,`Age Class`,`Reproductive Status`,`Study Period`,Year)%>%
+  mutate(Sex=ifelse(Sex=="Male","M","F"))
+colnames(stats)<-c("id","coug_sex","coug_age","kits","study_period","year")
+
+#join demographics and full data
+c15_19$id<-gsub(" ", "", as.character(c15_19$id)) #remove space from id
+full_dir<-left_join(c15_19,stats)%>%
+  mutate(species=case_when(
+    species=="Elk"~"elk",
+    species=="Deer (unknown)"~"unk deer",
+    species=="Deer (mule)"~"mule deer",
+    species=="Deer (whitetailed)"~"wt deer",
+    species=="Porcupine"~"porcupine",
+    species=="Bighorn sheep"~"bh sheep",
+    species=="Other"~"other",
+    species=="MULE DEER"~"mule deer",
+    species=="ELK"~"elk",
+    species=="PORCUPINE"~"porcupine"))%>%
+  mutate(age=case_when(
+    age=="Calf/fawn"~"fawn",
+    age=="Unknown"~"unk",
+    age=="Yearling"~"yearling",
+    age=="Old adult (10+ yrs)"~"old adult",
+    age=="Adult"~"adult",
+    age=="FAWN"~"fawn",
+    age=="ADULT"~"adult",
+    age=="YEARLING"~"yearling",
+    age=="CALF"~"fawn"
+  ))
+full_dir$utm_e<-gsub("0532908", "532908",
+                    as.character(full_dir$utm_e))
+
+write.csv(full_dir,here("data/full_dir.csv"))
+#still need to define early/mid/late winter
