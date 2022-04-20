63c9ecd450139ef280205a40e3aec28d45712b20
tamta117 <tamtar1@uw.edu>
2022-04-20 16:53:13 -0700

    more preliminary plots

--- scripts/prelim_plots.R
+++ scripts/prelim_plots.R
@@ -14,10 +14,10 @@ age<-dir%>%
 
 age_jday<-dir%>%
   subset(dir$species=="elk" | dir$species=="mule deer")%>%
-  group_by(jday,age)%>%
+  group_by(jday,age,season)%>%
   summarize(nobs=n())
 
-ggplot(season)+
+ggplot(age)+
   geom_bar(aes(x=season,y=nobs,fill=species),stat="identity",
            position=position_dodge(0.9))
 
@@ -28,3 +28,8 @@ ggplot(age)+
 ggplot(age_jday)+
   geom_bar(aes(x=jday,y=nobs,fill=age),stat="identity",
               position=position_dodge(0.9))
+
+ggplot(age_jday%>%
+         filter(season=="winter"))+
+  geom_tile(aes(x=age,y=jday,fill=nobs),stat="identity",
+           position=position_dodge(0.9))
