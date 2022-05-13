32ce7639e48f0700bd684cd1ae5da27eb6e2859b
tam.le.ta2001@gmail.com <tamtar1@uw.edu>
2022-05-12 20:18:05 -0700

    added plots

--- scripts/elevation_model_test.R
+++ scripts/elevation_model_test.R
@@ -3,6 +3,7 @@
 library(here)
 library(tidyverse)
 library(elevatr)
+library(ggplot2)
 
 #load data
 odata<-read.csv(here("data/all_elev.csv"))%>%
@@ -49,6 +50,8 @@ summary(mule.mod)
 elk.plot <- data.frame(elevation=seq(min(elk$elevation), max(elk$elevation),
                              len=500))
 elk.plot$pre = predict(elk.mod, elk.plot, type="response")
+elk.plot$min<-elk.plot$pre-0.16
+elk.plot$max<-elk.plot$pre+0.16
 ggplot(elk.plot, aes(x=elevation, y=pre)) + 
   geom_line()+
   ylab("Predicted probability \n of elk kill")+
@@ -58,7 +61,22 @@ ggplot(elk.plot, aes(x=elevation, y=pre)) +
 mule.plot <- data.frame(elevation=seq(min(mule$elevation), max(mule$elevation),
                                      len=500))
 mule.plot$pre = predict(mule.mod, mule.plot, type="response")
-ggplot(mule.plot, aes(x=elevation, y=pre)) + 
-  geom_line()+
+# mule.plot$min<-mule.plot$pre-0.1906
+# mule.plot$max<-mule.plot$pre+0.1906
+ggplot(mule.plot, aes(x=elevation, y=pre)) +
+  geom_smooth(size=1.5,color="black")+
+  geom_ribbon(aes(ymin=min,ymax=max),linetype=2,alpha=0.1)+
   ylab("Predicted probability \n of mule deer kill")+
   xlab("Elevation (m)")
+
+#plot joined model
+mule.plot$group<-"1"
+elk.plot$group<-"2"
+bind.plot<-rbind(mule.plot,elk.plot)
+ggplot(bind.plot, aes(x=elevation, y=pre, col=group)) +
+  #geom_ribbon(aes(ymin=min,ymax=max), fill="grey70")+
+  geom_smooth(size=1.5)+
+  xlab("Elevation (m)")+
+  ylab("Predicted probability of kill")+
+  scale_color_manual(values = c("#39638b","#26a185"), name = "Species",
+                     labels=c("Mule deer","Elk"))
